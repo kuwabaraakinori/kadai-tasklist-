57899ce252933ad246772b85321fc055f7b37808
nori <kuwabara.akinori.s0@tufs.ac.jp>
2020-06-04 10:30:36 +0000

    kadai-list

--- resources/views/task/edit.blade.php
+++ resources/views/task/edit.blade.php
@@ -2,14 +2,14 @@
 
 @section('content')
 
-<h1>id: {{ $tasks->id }} のメッセージ編集ページ</h1>
+<h1>id: {{ $tasks->id }} のタスクの編集ページ</h1>
 
     <div class="row">
         <div class="col-6">
             {!! Form::model($tasks, ['route' => ['tasks.update', $tasks->id], 'method' => 'put']) !!}
 
                 <div class="form-group">
-                    {!! Form::label('content', 'メッセージ:') !!}
+                    {!! Form::label('content', 'タスク:') !!}
                     {!! Form::text('content', null, ['class' => 'form-control']) !!}
                 </div>
 
